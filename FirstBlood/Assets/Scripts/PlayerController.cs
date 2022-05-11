d329111d783bc9b1cfe2d38169273ff185a1e12f
Maulem <49657965+Maulem@users.noreply.github.com>
2022-05-11 08:41:27 -0300

    Player Base done + WebGL switch

--- FirstBlood/Assets/Scripts/PlayerController.cs
+++ FirstBlood/Assets/Scripts/PlayerController.cs
@@ -3,20 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-   float _baseSpeed = 10.0f;
+    float _gravidade = 9.8f;
+    float _baseSpeed = 10.0f;
 
-   CharacterController characterController;
-   
-   void Start() {
-       characterController = GetComponent<CharacterController>();
-   }
+    //Referência usada para a câmera filha do jogador
+    GameObject playerCamera;
+    //Utilizada para poder travar a rotação no angulo que quisermos.
+    float cameraRotation;
 
-   void Update() {
-       float x = Input.GetAxis("Horizontal");
-       float z = Input.GetAxis("Vertical");
+    CharacterController characterController;
+    
+    void Start() {
+        characterController = GetComponent<CharacterController>();
 
-       Vector3 direction = new Vector3(x, 0, z);
+        playerCamera = GameObject.Find("Main Camera");
+        cameraRotation = 0.0f;
+    }
 
-       characterController.Move(direction * _baseSpeed * Time.deltaTime);
-   }
+    void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float y = 0;
+        float z = Input.GetAxis("Vertical");
+
+        //Verificando se é preciso aplicar a gravidade
+        if(!characterController.isGrounded) {
+            y = -_gravidade;
+        }
+
+        //Tratando movimentação do mouse
+        float mouse_dX = Input.GetAxis("Mouse X");
+        float mouse_dY = Input.GetAxis("Mouse Y");
+
+        //Tratando a rotação da câmera
+        cameraRotation -= mouse_dY;
+        cameraRotation = Mathf.Clamp(cameraRotation, -75.0f, 75.0f);
+
+        Vector3 direction = transform.right * x + transform.up * y + transform.forward * z;
+
+        characterController.Move(direction * _baseSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, mouse_dX);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
+    }
 }
